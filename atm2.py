fd8abc5d870484961f52a308c184bdb8b1e0a30f
Geonchang Lee <geonchang1110@gmail.com>
2021-09-22 18:43:28 +0900

    atm

--- atm2.py
+++ atm2.py
@@ -1,7 +1,8 @@
 from atm1 import atm
 atm = atm()
+print("통장 또는 카드를 삽입하여주십시오.")
 while True:
-    print("통장 또는 카드를 삽입하여주십시오.")
-    a = input("1. 입금\n2. 출금\n3. 송금\n 선택")
+
+    a = input("1. 입금\n2. 출금\n3. 송금\n 선택>> ")
     if a == "1":
         amount = input("입금하실 금액을 투입하여주십시오")
